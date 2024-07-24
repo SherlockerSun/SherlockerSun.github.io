43e3a8f7b2884701bdf3671ba40f6985fc56a9e2
SherlockerSun <936938041@qq.com>
2024-07-24 09:59:37 +0800

    Site updated: 2024-07-24 09:59:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/24/JVM调优/","2024/07/10/hello-world/","2024/07/15/面试总结/"];function toRandomPost(){
+var posts=["2024/07/15/面试总结/","2024/07/10/hello-world/","2024/07/24/JVM调优/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

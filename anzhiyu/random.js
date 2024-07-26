e5007982ab3a42e3989b53b5ff059c72d91ce22d
SherlockerSun <936938041@qq.com>
2024-07-26 15:43:43 +0800

    Site updated: 2024-07-26 15:43:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/25/Hello 算法/","2024/07/24/Hexo部署教程/","2024/07/24/JVM调优/","2024/07/25/Oracle调优之看懂Oracle执行计划/","2024/07/25/SpringBoot整合EMQX（MQTT协议）/","2024/07/25/SpringBoot整合Quartz/","2024/07/24/截图工具/","2024/07/15/面试总结/"];function toRandomPost(){
+var posts=["2024/07/25/Hello 算法/","2024/07/24/JVM调优/","2024/07/25/Oracle调优之看懂Oracle执行计划/","2024/07/24/Hexo部署教程/","2024/07/25/SpringBoot整合EMQX（MQTT协议）/","2024/07/24/截图工具/","2024/07/25/SpringBoot整合Quartz/","2024/07/15/面试总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
